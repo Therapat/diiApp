c03576a93d550124e57c54af0d39c6141b39f0f1
Thiraphat <Saratoga1412@gmial.com>
2020-08-03 15:09:24 +0700

    ADD file

---
+++ index.js
@@ -0,0 +1,96 @@
+// var doge = 10000 ;
+// var dd = [1,2,3,"Hello"];
+// console.log("Hello JS");
+
+// const student1 = [{
+//     name: "naratip" ,
+//     age : 20,
+//     "sex"  : "male",
+//     candy : [
+//         {nameCandy : "mindmint", 
+//         price : 1500}
+//     ]
+
+// }];
+// const student2 = {
+//     name: "keng " ,
+//     age : 19,
+//     sex  : "male",
+
+// };
+
+// var num = "5" ;
+// num = num * "5";
+
+// console.log(num);
+// console.log(student1[0].candy[0].nameCandy);
+
+// const doge = [1,2,3];
+
+// const num1 = doge[0];
+// const num2 = doge[1];
+// const num3 = doge[2];
+
+// const [num1,num2,num3] = doge;
+
+// const obj = {
+//     dd : 0,
+//     ff : 2
+// };
+
+// const {dd,ff} = obj;
+// const obj2 = {...obj};
+
+//forEach ใช้เพื่อวนลูปรอบอาร์เรย์ ส่วน for-in ใช้วนลูปรอบ key ของอ็อบเจ็กต์ ในขณะที่ for-of นั้นทรงพระเพาเวอร์มาก ใช้เพื่อวนลูปรอบ Iterable Objects โดยเราสามารถกำหนดพฤติกรรมการส่งออกค่าได้นั่นเอง
+//for(const a in obj)
+//for(const a of obj)
+//console.log(num1,num2);
+
+//function
+
+//ประกาศจาก value
+
+// let sum = (num1,num2) => {
+//     return num1,num2;
+// }
+
+// //ประกาศทั่วไป
+// function sum(num1,num2) {
+//     return num1,num2;
+// }
+
+// //เหมือนกัน
+// const func = para => para*5 ; //เอาค่า return มาปู้ยี่ปู้ยำ 
+// const numti = (para=1) => para*5 ; //setค่าpara
+// const numti = para => console.log(para) ; //displa para
+// //same 
+// function func(para =1 ) { 
+//     return para*5};
+
+// //set func รับค่า Obj
+// const someFunc = ({ name}= {} ) => {
+//     console.log(name);
+// }
+// //same
+// function someFunc (profile = {} ){
+//     const {name } = profile;
+//     console.log(name);
+
+// }
+
+//ถ้ามีหลายพารามิเตอร์ส่งมา
+// sometingFunc("name",1,2,3)
+// sometingFunc("name",1,2,3,4)
+// sometingFunc("name","A","B","C")
+
+// function sometingFunc(...para){
+//     console.log(para)
+// }
+
+
+
+
+
+
+
+
